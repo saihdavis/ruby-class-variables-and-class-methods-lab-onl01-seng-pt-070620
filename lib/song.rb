67d3e39fe6b8490d9d478572f276adb4e3b3b13e
Saida Davis <saihdavis@gmail.com>
2020-07-28 05:20:14 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,43 @@
 class Song 
   
-  attr_reader :name, :artist, :genre 
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  attr_accessor :name, :artist, :genre 
   
   def initialize(name, artist, genre)
+    @@count += 1 
     @name = name
     @artist = artist
     @genre = genre
+    @@genres << genre
+    @@artists << artist
+    
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
+  
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
+    artist_count
   end
   
 end
