2e34fc397e2ba06366c1d8e5b4575dfc4b62020a
Saida Davis <saihdavis@gmail.com>
2020-07-28 05:17:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   
   def self.genre_count
     genre_count = Hash.new("genre")
-    @@genres_count.each {|genre| genre_count[genre] += 1}
+    @@genres.each {|genre| genre_count[genre] += 1}
     genre_count
   end
 end
